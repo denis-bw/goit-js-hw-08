551431c2ea344883709e504dd409726b26640637
Denis Belousov <109822229+denis-bw@users.noreply.github.com>
2023-03-13 21:45:12 +0200

    hw_1_gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const galleryImg = galleryItems.map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+            </a>`;
+}).join('');
+
+galleryRef.innerHTML = galleryImg;
+
+
+new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+})
